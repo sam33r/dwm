5de6e1d045ea9b78f22d47a221e5f729479cc0f5
Sameer Ahuja <samahuja@google.com>
2018-09-18 13:17:09 -0700

    Update focusmaster function to also raise windows.

--- focusmaster.c
+++ focusmaster.c
@@ -1,4 +1,12 @@
 static void
 focusmaster(const Arg *arg) {
-	focus(nexttiled(selmon->clients));
+  unsigned int n;
+  Client *c;
+  for (n = 0, c = nexttiled(selmon->clients);
+      n < selmon->nmaster && c;
+      c = nexttiled(c->next), n++) {
+    if (n == 0)
+      focus(c);
+    XRaiseWindow(dpy, c->win);
+  }
 }
