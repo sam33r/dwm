94c08945de8368b9e72254a71ead31550b1e30e0
Sameer Ahuja <samahuja@google.com>
2020-02-07 18:00:39 -0800

    Make window focusing better.
    Now it works even if the window is behind an invisible tag in the other
    monitor.

--- dwm.c
+++ dwm.c
@@ -717,6 +717,10 @@ clientmessage(XEvent *e)
 			setfullscreen(c, (cme->data.l[0] == 1 /* _NET_WM_STATE_ADD    */
 				|| cme->data.l[0] == 2 /* _NET_WM_STATE_TOGGLE */));
 	} else if (cme->message_type == netatom[NetActiveWindow]) {
+    /* This only works because I never have more than two monitors. */
+    if (c->mon != selmon) {
+      focusmon(&arg);
+    }
    	for (i = 0; i < LENGTH(tags) && !((1 << i) & c->tags); i++);
 		if (i < LENGTH(tags)) {
 			const Arg a = {.ui = 1 << i};
