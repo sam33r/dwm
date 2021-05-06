4ec1f4a82e9c02709844b4de55222868790c1103
Sameer <sameer.ahuja@gmail.com>
2021-05-05 23:43:07 -0700

    Add window placement rule for VSCode.

--- config.h
+++ config.h
@@ -62,6 +62,7 @@ static const Rule rules[] = {
   { "Google-chrome",      NULL,         NULL,  1 << 1,   0,         0,         -1 },
   { "Chromium",           NULL,         NULL,  1 << 1,   0,         0,         -1 },
   { NULL,                 "emacs",      NULL,  1 << 2,   0,         0,         -1 },
+  { "Code",               NULL,         NULL,  1 << 2,   0,         0,         -1 },
 	{ "copyq",              NULL,         NULL,  ~0,       1,         1,         -1 },
 	{ "scrcpy",             NULL,         NULL,  1 << 4,   0,         1,         -1 },
 	{ NULL,                 "emacs",      "background-task",  1 << 4, 0, 0,      -1 },
