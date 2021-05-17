2da7bdf65c59f6e636bbf2e8d4b427e8dcd4998d
Sameer <sameer.ahuja@gmail.com>
2021-05-17 00:34:06 -0700

    Set the window manager hints on tab bar.

--- dwm.c
+++ dwm.c
@@ -2766,6 +2766,7 @@ updatebars(void)
                                   				  CWOverrideRedirect|CWBackPixmap|CWEventMask, &wa);
     	XDefineCursor(dpy, m->tabwin, cursor[CurNormal]->cursor);
     	XMapRaised(dpy, m->tabwin);
+		  XSetClassHint(dpy, m->tabwin, &ch);
 	}
 }
 
